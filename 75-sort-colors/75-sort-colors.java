f74ce32ab404c1a1a6bc705a48eafcddfe75a421
Aman kumar <80037371+Amantomar81@users.noreply.github.com>
2022-08-17 10:49:21 +0530

    Time: 0 ms (100.00%), Space: 41 MB (88.98%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -4,7 +4,6 @@ class Solution {
         int n = nums.length;
         int low =0 , mid =0;
         int high = n-1;
-        
         while(mid <= high){
             if(nums[mid] == 0){
                 swap(nums,low,mid);
