27dbf5242592d802dc6beb4701f180c6bfe5f4eb
Aman kumar <80037371+Amantomar81@users.noreply.github.com>
2022-09-06 10:32:43 +0530

    Time: 9 ms (51.38%), Space: 46 MB (21.94%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,19 +1,31 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
-        int arr[] = new int[2];
+//         int arr[] = new int[2];
         
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                int sum = nums[i] + nums[j];
+//         for(int i=0;i<nums.length;i++){
+//             for(int j=i+1;j<nums.length;j++){
+//                 int sum = nums[i] + nums[j];
                 
-                if(sum == target){
-                   arr[0] = i;
-                    arr[1] = j;
+//                 if(sum == target){
+//                    arr[0] = i;
+//                     arr[1] = j;
                     
-                }
+//                 }
+//             }
+//         }
+//         return arr;
+        
+        HashMap<Integer, Integer>map = new HashMap<Integer, Integer>();
+        for(int i=0;i<nums.length;i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[] {map.get(complement),i};
+            }
+            else{
+                map.put(nums[i],i);
             }
         }
-        return arr;
+        throw new IllegalArgumentException("No two sun solution");
     }
 }
